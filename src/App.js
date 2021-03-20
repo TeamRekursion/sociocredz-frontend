cf669241c70ff6498433366fff1ce0824b2cd0c2
Your Name <nirmitjatana38@gmail.com>
2021-03-21 01:26:57 +0530

    feat: react router added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
-// import Landingpage from './pages/landing'
-// import Loginpage from './pages/loginpage';
-// import More from './pages/more';
-import Dashnav from './components/dashnav';
+import Landingpage from './pages/landing'
+import Loginpage from './pages/loginpage';
+import More from './pages/more';
+import { BrowserRouter as Router, Switch , Route} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
+// import Dashnav from './components/dashnav';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Dashnav/>
+      <Switch>
+        <Route path="/" exact component={Landingpage}/>
+        <Route path="/login" component={Loginpage}/>
+        <Route path="/more" component={More}/>
+      </Switch>
+      {/* <Dashnav/> */}
       {/* <Loginpage/> */}
       {/* <More/> */}
       {/* <Landingpage/> */}
     </div>
+    </Router>
   );
 }
 
