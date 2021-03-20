90e95dc983e9b6f70b570a97f4cb4a48a3d9b91c
Your Name <nirmitjatana38@gmail.com>
2021-03-21 03:22:40 +0530

    feat: campaign form

--- src/pages/campaign.js
+++ src/pages/campaign.js
@@ -18,10 +18,38 @@ class Landingpage extends React.Component {
         <div className="grid-set">
           <div className="leftg">
             <h3 style={{color:"#fc7b22"}}>Start a Campaign</h3>
-            <h6>Campaigns allow you to crowdfund your charity events.</h6>
+            <h6 className="him">Campaigns allow you to crowdfund your charity events.</h6>
+            <form>
+              <label className='loglab'>
+                Campaign Title
+              </label>
+              <input className='logpu'/>
+              <br/>
+              <br/>
+              <label className='loglab'>
+                Campaign Tagline
+              </label>
+              <input className='logpu'/>
+              <br/>
+              <br/>
+              <label className='loglab'>
+                Campaign Description
+              </label>
+              <input className='logpu'/>
+              <br/>
+              <br/>
+              <label className='loglab'>
+                Target money to be raised
+              </label>
+              <input className='logpu'/>
+              <button type="submit">
+                Start Campaign
+              </button>
+            </form>
           </div>
           <div className="rightg">
-            right
+          <h3 style={{color:"#fc7b22"}}>Start a Campaign</h3>
+            <h6 className="him">Campaigns allow you to crowdfund your charity events.</h6>
           </div>
         </div>
       </div>
