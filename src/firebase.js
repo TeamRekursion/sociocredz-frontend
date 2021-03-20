0ccce3b65df75a2de42719c9f970658b3c70c66e
Your Name <nirmitjatana38@gmail.com>
2021-03-20 23:06:54 +0530

    feat: firebase auth complete

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import * as firebase from "firebase"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBLXwEsbaAuOUr9N_Q9kpBVY-vSWEjm8zs",
-    authDomain: "sociocredz.firebaseapp.com",
-    projectId: "sociocredz",
-    storageBucket: "sociocredz.appspot.com",
-    messagingSenderId: "89086579919",
-    appId: "1:89086579919:web:18b3c28c0aa1e6332653ba",
-    measurementId: "G-R7SDQYPDJR"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  apiKey: "AIzaSyBLXwEsbaAuOUr9N_Q9kpBVY-vSWEjm8zs",
+  authDomain: "sociocredz.firebaseapp.com",
+  projectId: "sociocredz",
+  storageBucket: "sociocredz.appspot.com",
+  messagingSenderId: "89086579919",
+  appId: "1:89086579919:web:18b3c28c0aa1e6332653ba",
+  measurementId: "G-R7SDQYPDJR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -1,21 +1,69 @@
 import React from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
 
 class Loginpage extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            idToken : null,
+        };
+      }
+      firebasetoken(){
+        var cb = (x)=>{
+            this.setState({idToken : x});
+            // console.log(this.state.idToken);
+        };
+        var firebaseConfig = {
+        apiKey: "AIzaSyBLXwEsbaAuOUr9N_Q9kpBVY-vSWEjm8zs",
+        authDomain: "sociocredz.firebaseapp.com",
+        projectId: "sociocredz",
+        storageBucket: "sociocredz.appspot.com",
+        messagingSenderId: "89086579919",
+        appId: "1:89086579919:web:18b3c28c0aa1e6332653ba",
+        measurementId: "G-R7SDQYPDJR"
+        };
+
+        firebase.initializeApp(firebaseConfig);
+
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/userinfo.profile')
+            firebase.auth().signInWithPopup(provider).then(function (result) {
+                var user = result.user;
+                user.getIdToken().then((data) => {
+                    cb(data);
+
+
+                    console.log(data);
+                    postRequest('https://sociocredz.herokuapp.com/api/v1/ngo/login', {idToken: data})
+                    .then(data => console.log(data)) // Result from the `response.json()` call
+                    .catch(error => console.error(error))
+
+                    function postRequest(url, data) {
+                    return fetch(url, {
+                        credentials: 'same-origin', // 'include', default: 'omit'
+                        method: 'POST', // 'GET', 'PUT', 'DELETE', etc.
+                        body: JSON.stringify(data), // Coordinate the body type with 'Content-Type'
+                        headers: new Headers({
+                        'Content-Type': 'application/json'
+                        }),
+                    })
+                    .then(response => response.json())
+                }        
+
+                })
+            })
       }
 
-      componentDidMount() {
-      }
+    //   componentDidMount() {
+    //   }
       render() {
         return(
           <div className="logcont">
               <div className="left">left column</div>
               <div className="rig">
                   <h2>Sign in to Sociocredz!</h2>
-                  <br/>
-                  <button className="signu">G<span style={{color:"#FC9F25"}}>......</span>|<span style={{color:"#FC9F25"}}>.....</span>    Sign in with Google</button>
+                  <button className="signu" onClick={() => this.firebasetoken()}>G<span style={{color:"#FC9F25"}}>......</span>|<span style={{color:"#FC9F25"}}>.....</span>    Sign in with Google</button>
                   <div className="or">OR</div>
                   <form>
                       <label className="loglab">Email ID</label>
